e77dde99788ab28c48b0268f1edcba491bac69a1
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-10-22 14:29:40 +0100

    finished cleaning script

---
+++ cleaning_scripts/cleaning_script.R
@@ -0,0 +1,141 @@
+##########libraries#############
+
+
+library(tidyverse) 
+library(readxl)
+library(here)
+library(zoo)
+
+
+##########1#############
+
+responses_1 <- read_xlsx("raw_data/Reaesrch project - questionnaire responses (anon).xlsx",
+                        sheet = "Pre Section 1 (About you)",
+                        col_names = TRUE) %>% 
+    select(-1) %>% 
+    head(.,-1)
+
+
+colnames(responses_1)[str_detect(colnames(responses_1),"[...]")] <- NA
+
+colnames(responses_1) <- colnames(responses_1) %>% 
+    zoo::na.locf(na.rm = TRUE) 
+
+names(responses_1) <- paste(names(responses_1), responses_1[1, ], sep = "_")
+
+tidy_data_1 <- responses_1 %>% 
+    slice(-1) %>% 
+    rowid_to_column( "survey_id") %>%
+    pivot_longer(
+        cols = -survey_id,
+        names_to = c("question", "answer"),
+        names_sep = "_",
+        values_to = "value",
+        values_drop_na = TRUE
+    ) %>% 
+mutate(survey_time = "pre",
+       survey_set = "about_you") %>%
+    relocate(c(survey_time, survey_set), .after = survey_id)
+
+
+
+##########2#############
+
+responses_2 <- read_xlsx("raw_data/Reaesrch project - questionnaire responses (anon).xlsx",
+                        sheet = "Pre Section 2 (Your beliefs)",
+                        col_names = TRUE) %>% 
+    select(-1) %>% 
+    head(.,-1)
+
+
+colnames(responses_2)[str_detect(colnames(responses_2),"[...]")] <- NA
+
+colnames(responses_2) <- colnames(responses_2) %>% 
+    zoo::na.locf(na.rm = TRUE) 
+
+names(responses_2) <- paste(names(responses_2), responses_2[1, ], sep = "_")
+
+tidy_data_2<- responses_2 %>% 
+    slice(-1) %>% 
+    rowid_to_column( "survey_id") %>%
+    pivot_longer(
+        cols = -survey_id,
+        names_to = c("question", "answer"),
+        names_sep = "_",
+        values_to = "value",
+        values_drop_na = TRUE
+    ) %>% 
+    mutate(survey_time = "pre",
+           survey_set = "your_beliefs") %>%
+    relocate(c(survey_time, survey_set), .after = survey_id) %>% 
+    slice(-1)
+
+
+##########3#############
+
+responses_3 <- read_xlsx("raw_data/Reaesrch project - questionnaire responses (anon).xlsx",
+                        sheet = "Post Section 1 (About you)",
+                        col_names = TRUE) %>% 
+    select(-1) %>% 
+    head(.,-1)
+
+
+colnames(responses_3)[str_detect(colnames(responses_3),"[...]")] <- NA
+
+colnames(responses_3) <- colnames(responses_3) %>% 
+    zoo::na.locf(na.rm = TRUE) 
+
+names(responses_3) <- paste(names(responses_3), responses_3[1, ], sep = "_")
+
+tidy_data_3 <- responses_3 %>% 
+    slice(-1) %>% 
+    rowid_to_column( "survey_id") %>%
+    pivot_longer(
+        cols = -survey_id,
+        names_to = c("question", "answer"),
+        names_sep = "_",
+        values_to = "value",
+        values_drop_na = TRUE
+    ) %>% 
+    mutate(survey_time = "post",
+           survey_set = "about_you") %>%
+    relocate(c(survey_time, survey_set), .after = survey_id) %>% 
+    slice(-1)
+
+
+##########4#############
+
+responses_4 <- read_xlsx("raw_data/Reaesrch project - questionnaire responses (anon).xlsx",
+                        sheet = "Post Section 2 (Your beliefs)",
+                        col_names = TRUE) %>% 
+    select(-1) %>% 
+    head(.,-1)
+
+
+colnames(responses_4)[str_detect(colnames(responses_4),"[...]")] <- NA
+
+colnames(responses_4) <- colnames(responses_4) %>% 
+    zoo::na.locf(na.rm = TRUE) 
+
+names(responses_4) <- paste(names(responses_4), responses_4[1, ], sep = "_")
+
+tidy_data_4 <- responses_4 %>% 
+    slice(-1) %>% 
+    rowid_to_column( "survey_id") %>%
+    pivot_longer(
+        cols = -survey_id,
+        names_to = c("question", "answer"),
+        names_sep = "_",
+        values_to = "value",
+        values_drop_na = TRUE
+    ) %>% 
+    mutate(survey_time = "post",
+           survey_set = "your_beliefs") %>%
+    relocate(c(survey_time, survey_set), .after = survey_id) %>% 
+    slice(-1)
+
+######Combined data#####
+
+rbind(tidy_data_1, tidy_data_2, tidy_data_3, tidy_data_4) %>% 
+    write_csv("clean_data/summary_data.csv")
+    
