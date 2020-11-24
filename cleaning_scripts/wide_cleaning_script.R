f032672fdc2a178410b799bee52a1c018ee59cf2
Mightyjohnny <59116056+Mightyjohnny@users.noreply.github.com>
2020-11-24 00:44:08 +0000

    fixed NA bug

--- cleaning_scripts/wide_cleaning_script.R
+++ cleaning_scripts/wide_cleaning_script.R
@@ -13,7 +13,7 @@ responses_1 <- read_xlsx("raw_data/Reaesrch project - questionnaire responses (a
                         sheet = "Pre Section 1 (About you)",
                         col_names = TRUE) %>% 
     select(-1) %>% 
-    head(.,-1)
+    head(-1)
 
 
 colnames(responses_1)[str_detect(colnames(responses_1),"[...]")] <- NA
@@ -26,7 +26,7 @@ names(responses_1) <- paste(names(responses_1), responses_1[1, ], sep = "_")
 tidy_data_1 <-
     responses_1 %>% 
     slice(-1) %>% 
-    rowid_to_column( "survey_id") %>%
+    rowid_to_column("survey_id") %>%
     pivot_longer(
         cols = -survey_id,
         names_to = c("question"
@@ -34,7 +34,7 @@ tidy_data_1 <-
         ),
         names_sep = "_",
         values_to = "value",
-        values_drop_na = FALSE
+        values_drop_na = TRUE
     ) %>% 
     mutate(survey_time = "pre",
            survey_set = "about_you") %>%
@@ -50,20 +50,20 @@ responses_2 <- read_xlsx("raw_data/Reaesrch project - questionnaire responses (a
                         sheet = "Pre Section 2 (Your beliefs)",
                         col_names = TRUE) %>% 
     select(-1) %>% 
-    head(.,-1) %>% 
+    head(-1) %>%
     view()
 
 
 colnames(responses_2)[str_detect(colnames(responses_2),"[...]")] <- NA
 
 colnames(responses_2) <- colnames(responses_2) %>% 
-    zoo::na.locf(na.rm = TRUE) 
+    zoo::na.locf(na.rm = FALSE) 
 
 names(responses_2) <- paste(names(responses_2), responses_2[1, ], sep = "_")
 
 tidy_data_2<- responses_2 %>% 
     slice(-1) %>% 
-    rowid_to_column( "survey_id") %>%
+    rowid_to_column("survey_id") %>%
     pivot_longer(
         cols = -survey_id,
         names_to = c("question"
@@ -71,13 +71,14 @@ tidy_data_2<- responses_2 %>%
         ),
         names_sep = "_",
         values_to = "value",
-        values_drop_na = FALSE
+        values_drop_na = TRUE
     ) %>% 
     mutate(survey_time = "pre",
            survey_set = "you_beliefs") %>%
     relocate(c(survey_time, survey_set), .after = survey_id) %>% 
     filter(value == 1) %>% 
-    select(-value)
+    select(-value) %>% 
+    view()
 
 
 ##########3#############
@@ -86,19 +87,19 @@ responses_3 <- read_xlsx("raw_data/Reaesrch project - questionnaire responses (a
                         sheet = "Post Section 1 (About you)",
                         col_names = TRUE) %>% 
     select(-1) %>% 
-    head(.,-1)
+    head(-1)
 
 
 colnames(responses_3)[str_detect(colnames(responses_3),"[...]")] <- NA
 
 colnames(responses_3) <- colnames(responses_3) %>% 
-    zoo::na.locf(na.rm = TRUE) 
+    zoo::na.locf(na.rm = FALSE) 
 
 names(responses_3) <- paste(names(responses_3), responses_3[1, ], sep = "_")
 
 tidy_data_3 <- responses_3 %>% 
     slice(-1) %>% 
-    rowid_to_column( "survey_id") %>%
+    rowid_to_column("survey_id") %>%
     pivot_longer(
         cols = -survey_id,
         names_to = c("question"
@@ -106,7 +107,7 @@ tidy_data_3 <- responses_3 %>%
         ),
         names_sep = "_",
         values_to = "value",
-        values_drop_na = FALSE
+        values_drop_na = TRUE
     ) %>% 
     mutate(survey_time = "post",
            survey_set = "about_you") %>%
@@ -118,22 +119,23 @@ tidy_data_3 <- responses_3 %>%
 ##########4#############
 
 responses_4 <- read_xlsx("raw_data/Reaesrch project - questionnaire responses (anon).xlsx",
-                        sheet = "Post Section 2 (Your beliefs)",
-                        col_names = TRUE) %>% 
+                         sheet = "Pre Section 2 (Your beliefs)",
+                         col_names = TRUE) %>% 
     select(-1) %>% 
-    head(.,-1)
+    head(-1) %>% 
+    view()
 
 
 colnames(responses_4)[str_detect(colnames(responses_4),"[...]")] <- NA
 
 colnames(responses_4) <- colnames(responses_4) %>% 
-    zoo::na.locf(na.rm = TRUE) 
+    zoo::na.locf(na.rm = FALSE) 
 
 names(responses_4) <- paste(names(responses_4), responses_4[1, ], sep = "_")
 
-tidy_data_4 <- responses_4 %>% 
+tidy_data_4<- responses_4 %>% 
     slice(-1) %>% 
-    rowid_to_column( "survey_id") %>%
+    rowid_to_column("survey_id") %>%
     pivot_longer(
         cols = -survey_id,
         names_to = c("question"
@@ -141,10 +143,10 @@ tidy_data_4 <- responses_4 %>%
         ),
         names_sep = "_",
         values_to = "value",
-        values_drop_na = FALSE
+        values_drop_na = TRUE
     ) %>% 
     mutate(survey_time = "post",
-           survey_set = "your_beliefs") %>%
+           survey_set = "you_beliefs") %>%
     relocate(c(survey_time, survey_set), .after = survey_id) %>% 
     filter(value == 1) %>% 
     select(-value)
@@ -155,29 +157,30 @@ rbind(tidy_data_1, tidy_data_2, tidy_data_3, tidy_data_4) %>%
     # view() %>% 
         pivot_wider(names_from = question, values_from = answer) %>% 
     unnest(-survey_id:-survey_set) %>% 
+    view()
     write_csv("clean_data/summary_data_wide.csv")
     
-test_1 <-tidy_data_1 %>% 
-    # view() %>% 
-    pivot_wider(names_from = question, values_from = answer) %>% 
-    unnest(-survey_id:-survey_set) %>% 
-    view()
-
-
-test_2 <-tidy_data_2 %>% 
-    # view() %>% 
-    pivot_wider(names_from = question, values_from = answer) %>% 
-    unnest(-survey_id:-survey_set) %>% 
-    view()
-
-test_3 <-tidy_data_3 %>% 
-    # view() %>% 
-    pivot_wider(names_from = question, values_from = answer) %>% 
-    unnest(-survey_id:-survey_set) %>% 
-    view()
-
-test_4 <-tidy_data_4 %>% 
-    # view() %>% 
-    pivot_wider(names_from = question, values_from = answer) %>% 
-    unnest(-survey_id:-survey_set) %>% 
-    view()
+# test_1 <-tidy_data_1 %>% 
+#     # view() %>% 
+#     pivot_wider(names_from = question, values_from = answer) %>% 
+#     unnest(-survey_id:-survey_set) %>% 
+#     view()
+# 
+# 
+# test_2 <-tidy_data_2 %>% 
+#     # view() %>% 
+#     pivot_wider(names_from = question, values_from = answer) %>% 
+#     unnest(-survey_id:-survey_set) %>% 
+#     view()
+# 
+# test_3 <-tidy_data_3 %>% 
+#     # view() %>% 
+#     pivot_wider(names_from = question, values_from = answer) %>% 
+#     unnest(-survey_id:-survey_set) %>% 
+#     view()
+# 
+# test_4 <-tidy_data_4 %>% 
+#     # view() %>% 
+#     pivot_wider(names_from = question, values_from = answer) %>% 
+#     unnest(-survey_id:-survey_set) %>% 
+#     view()
